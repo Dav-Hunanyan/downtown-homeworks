489f88c855b3382e98c5f4a7feb1d30721e219b3
Dav <dav040201@gmail.com>
2021-11-07 15:00:51 +0400

    Serialization and Deserialization for Employee class

---
+++ Xml/Xml/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Xml
+{
+    public class Employee
+    {
+        [XmlAttribute("Id")]
+        public int Id;
+        [XmlAttribute("name")]
+        public String name;
+       
+        public Employee()
+        {
+
+        }
+        public Employee(int id,string Name)
+        {
+            Id = id;
+            name = Name;        
+        }
+    }
+}
---
+++ Xml/Xml/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Xml
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string xml = @"F:\employee.txt";
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Employee));
+            Employee employee = new Employee(103221, "AAAAAAAAAAA");
+            // TextWriter txtWriter = new StreamWriter(xml);
+            //  xmlSerializer.Serialize(txtWriter, employee);
+
+            StreamReader streamReader = new StreamReader(xml);
+            Employee employee1 = (Employee)xmlSerializer.Deserialize(streamReader);
+        }
+
+
+
+
+    }
+}
